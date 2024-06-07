0809bc5c9eeec1d7397000bfae90a0c6eca2cc38
Ploc300 <ploc300@hotmail.com>
2024-06-07 20:17:43 +0200

    pylint valid

--- src/main.py
+++ src/main.py
@@ -9,7 +9,10 @@ import flask
 app = flask.Flask(__name__)
 
 @app.route('/')
-def index():
+def index() -> str:
+    """Index page for the app
+
+    Returns:
+        str: Hello, World!
+    """
     return 'Hello, World!'
-
-
